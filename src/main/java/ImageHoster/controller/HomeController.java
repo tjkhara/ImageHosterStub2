1d1e839adf6d195ad3642773dfddcf755497f8d2
Tajeshwar Khara <tkhara@gmail.com>
2019-12-14 17:47:20 +0530

    Stub 2 initial code

---
+++ src/main/java/ImageHoster/controller/HomeController.java
@@ -0,0 +1,24 @@
+package ImageHoster.controller;
+
+import ImageHoster.model.Image;
+import ImageHoster.service.ImageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    //Write the code to inject the ImageService bean
+
+    @RequestMapping("/")
+    public String getAllImages(Model model) {
+        //Call getAllImages() method in ImageService class to get the list of all images
+        //Add the list of images in the model with the key as "images"
+
+        return "index";
+    }
+}
---
+++ src/main/java/ImageHoster/model/Image.java
@@ -0,0 +1,24 @@
+package ImageHoster.model;
+import java.util.Date;
+
+public class Image {
+
+    //id of the image
+    private Integer id;
+
+    //title of the image
+    private String title;
+
+    //The image in Base64 format
+    private String imageFile;
+
+    //Description of the image
+    private String description;
+
+    //Date on which the image is posted
+    private Date date;
+
+    //Write the constructor for id, title, imageFile, and date
+
+    //Write getter and setter for all the attributes
+}
